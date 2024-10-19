6cebee0f3d64c9091601297c1af91cf131124e6f
JamieStewart4 <jamie.stewart@unb.ca>
2024-10-19 17:41:27 -0300

    Add select mode

---
+++ app/src/main/java/com/example/curlingclubchampions/SelectMode.kt
@@ -0,0 +1,11 @@
+package com.example.curlingclubchampions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectMode: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.select_mode)
+    }
+}
