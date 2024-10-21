e6307931a28225e5fd76d1d7327fe70d41dab996
masonlalonde17 <74019181+masonlalonde17@users.noreply.github.com>
2024-10-21 16:19:58 -0300

    Puzzle Mode
    created puzzle mode screen and view and activity

---
+++ app/src/main/java/com/example/curlingclubchampions/PuzzleMode.kt
@@ -0,0 +1,11 @@
+package com.example.curlingclubchampions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PuzzleMode: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.puzzle_mode)
+    }
+}
