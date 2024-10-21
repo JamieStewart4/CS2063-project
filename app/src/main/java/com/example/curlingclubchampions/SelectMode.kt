6d92a91cd3949e955def351843938e4c13424dbe
JamieStewart4 <jamie.stewart@unb.ca>
2024-10-21 17:27:11 -0300

    Setup title to select mode to puzzle mode buttons

--- app/src/main/java/com/example/curlingclubchampions/SelectMode.kt
+++ app/src/main/java/com/example/curlingclubchampions/SelectMode.kt
@@ -1,11 +1,19 @@
 package com.example.curlingclubchampions
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectMode: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.select_mode)
+
+        val puzzleButton = findViewById<Button>(R.id.puzzle_button)
+        puzzleButton.setOnClickListener {
+            val intent = Intent(this@SelectMode, PuzzleMode::class.java)
+            startActivity(intent)
+        }
     }
 }
