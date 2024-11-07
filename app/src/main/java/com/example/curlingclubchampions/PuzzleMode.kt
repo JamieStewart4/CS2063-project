1b244d1c9452fcd8f3a148117c3dd46e60ab6a95
JamieStewart4 <jamie.stewart@unb.ca>
2024-11-06 21:57:01 -0400

    Add rock reading functionality

--- app/src/main/java/com/example/curlingclubchampions/PuzzleMode.kt
+++ app/src/main/java/com/example/curlingclubchampions/PuzzleMode.kt
@@ -1,14 +1,27 @@
 package com.example.curlingclubchampions
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.example.curlingclubchampions.Rock.RockReader
 
 class PuzzleMode: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.puzzle_mode)
+        val layout = findViewById<RelativeLayout>(R.id.puzzle_relative_layout)
+
+        if (layout == null) {
+            Log.e("PuzzleMode", "Layout NULL!")
+        } else {
+            Log.i("PuzzleMode", "Layout NOT NULL!!!!")
+        }
 
         // Temp solve button for demo
         val solveButton = findViewById<Button>(R.id.temp_solve_button)
@@ -16,6 +29,25 @@ class PuzzleMode: AppCompatActivity() {
             val intent = Intent(this@PuzzleMode, PuzzleComplete::class.java)
             finish()
             startActivity(intent)
+        }
+
+        val rockReader = RockReader()
+        val jsonString = rockReader.readJSON(this, R.raw.level_1)
+        val rockList = rockReader.parseJSONToList(jsonString)
+
+        val redRockDrawable: Drawable? = getDrawable(R.drawable.red_rock)
+        val yellowRockDrawable: Drawable? = getDrawable(R.drawable.yellow_rock)
+
+        for (rock in rockList) {
+            val newRock = ImageView(this)
+            newRock.x = rock.x
+            newRock.y = rock.y
+            if (rock.colour == RockReader.Colour.RED) {
+                newRock.setImageDrawable(redRockDrawable)
+            } else if (rock.colour == RockReader.Colour.YELLOW) {
+                newRock.setImageDrawable(yellowRockDrawable)
+            }
+            layout.addView(newRock)
         }
     }
 }
---
+++ app/src/main/java/com/example/curlingclubchampions/Rock/RockReader.kt
@@ -0,0 +1,39 @@
+package com.example.curlingclubchampions.Rock
+
+import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
+
+
+class RockReader {
+
+    // Rock object constructor
+    // color = 0 -> Red
+    // color = 1 -> Yellow
+    data class Rock (
+        var colour: Colour,
+        var x: Float,
+        var y: Float
+    )
+
+    enum class Colour {
+        RED, YELLOW
+    }
+
+    // Wrapper class to take array from rocks object in json file
+    data class RockListWrapper(val rocks: List<Rock>)
+
+    // Read contents of json file into a string
+    fun readJSON(context: Context, resourceId: Int): String {
+        val inputStream = context.resources.openRawResource(resourceId)
+        return inputStream.bufferedReader().use { it.readText() }
+    }
+
+    // Parse lines of json string to create a list of rocks
+    fun parseJSONToList(jsonString: String): List<Rock> {
+        val gson = Gson()
+        val rockListWrapper = gson.fromJson(jsonString, RockListWrapper::class.java)
+        return rockListWrapper.rocks
+    }
+
+}
